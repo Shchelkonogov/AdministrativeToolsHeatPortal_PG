0a1b574ab5eea226f880d9fa2b982c3436c1f083
Maksim <shchelkonogov@mail.ru>
2021-07-30 12:52:45 +0300

    Добавил класс обработки ошибок выполнения функций базы для сис. пар.

---
+++ src/main/java/ru/tecon/admTools/systemParams/SystemParamException.java
@@ -0,0 +1,12 @@
+package ru.tecon.admTools.systemParams;
+
+/**
+ * Класс для обработки ошибок о невыполенние функций базы для формы системные параметры
+ * @author Maksim Shchelkonogov
+ */
+public class SystemParamException extends Exception {
+
+    public SystemParamException(String message) {
+        super(message);
+    }
+}
