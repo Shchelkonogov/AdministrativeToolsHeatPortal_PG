f977d46a5a61d9282192d18ded88cb302165541c
Maksim <shchelkonogov@mail.ru>
2023-02-08 14:47:15 +0300

    Удалил форму Системные свойства

--- src/main/java/ru/tecon/admTools/systemParams/cdi/SystemParamsMB.java
+++ src/main/java/ru/tecon/admTools/systemParams/cdi/SystemParamsMB.java
@@ -35,7 +35,6 @@ public class SystemParamsMB implements Serializable {
             {"Агрегаты", "/view/sysParams/struct/structAggregates.xhtml"},
             {"Техпроцессы", "/view/sysParams/struct/structProcesses.xhtml"},
 //            {"Устройства", "/view/sysParams/struct/structDevices.xhtml"},
-//            {"Системные свойства", "/view/sysParams/sysProp.xhtml"},
 //            {"Единицы измерения", "/view/sysParams/measure.xhtml"},
 //            {"Справочники", "/view/sysParams/catalog.xhtml"},
 //            {"Значения по умолчанию", "/view/sysParams/defaultValues.xhtml"},
