31efed8184fc62eb7457766f1c2e5e1c54e9784c
Maksim <shchelkonogov@mail.ru>
2023-03-24 12:30:59 +0300

    Улучшил проверку параметров на форме системные параметры

--- src/main/java/ru/tecon/admTools/systemParams/servlet/Servlet.java
+++ src/main/java/ru/tecon/admTools/systemParams/servlet/Servlet.java
@@ -3,12 +3,16 @@ package ru.tecon.admTools.systemParams.servlet;
 import ru.tecon.admTools.specificModel.ejb.CheckUserSB;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Сервлет обертка для запуска формы системные параметры
@@ -17,15 +21,34 @@ import java.io.IOException;
 @WebServlet("/systemParams")
 public class Servlet extends HttpServlet {
 
+    @Inject
+    private Logger logger;
+
     @EJB
     private CheckUserSB bean;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String sessionID = req.getParameter("sessionID");
-        if ((sessionID != null) && (bean.checkSession(sessionID))) {
-            req.getRequestDispatcher("/view/sysParams/systemParams.xhtml").forward(req, resp);
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        if (parameterMap.containsKey("sessionID") && parameterMap.containsKey("formID") && parameterMap.containsKey("ip")) {
+            try {
+                int ignore = Integer.parseInt(req.getParameter("formID"));
+
+                if (bean.checkSession(req.getParameter("sessionID"))) {
+                    req.getRequestDispatcher("/view/sysParams/systemParams.xhtml").forward(req, resp);
+                } else {
+                    // Авторизайтесь в системе
+                    logger.log(Level.WARNING, "authorization error");
+                    req.getRequestDispatcher("/error.html").forward(req, resp);
+                }
+            } catch (NumberFormatException ex) {
+                // Не корректный параметр formID
+                logger.log(Level.WARNING, "invalid parameter \"formID\"");
+                req.getRequestDispatcher("/error.html").forward(req, resp);
+            }
         } else {
+            // Не хватает параметров
+            logger.log(Level.WARNING, "missing parameters");
             req.getRequestDispatcher("/error.html").forward(req, resp);
         }
     }
